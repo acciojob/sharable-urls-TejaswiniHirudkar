e4baf971ad0b2497109dc55ab33281e35238b2d0
Tejaswini Hirudkar <121488475+TejaswiniHirudkar@users.noreply.github.com>
2023-06-22 19:41:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // your code here
+function updateurl(){
+	let heading=document.getElementById('url');
+	let name=document.getElementById('name').value;
+	let year=document.getElementById('year').value;
+	var url='';
+
+	if(name!=='' && year===''){
+		url="https://localhost:8080/"+"?name="+name;
+	}
+	else if(name!=='' && year!==''){
+		url="https://localhost:8080/"+"?name="+name+"&year="+year;
+	}
+	else if(year!=='' && name===''){
+		url="https://localhost:8080/"+"?year="+year;
+	}
+	else if(name==='' && year===''){
+		url="https://localhost:8080/";
+	}
+	heading.textContent=url;
+}
+
